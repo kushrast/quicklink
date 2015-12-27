e6e03d7f264c21b9f20e564527e64918826aeabb
Kush Rastogi <krustey@gmail.com>
2015-12-27 02:29:55 -0800

    Basic setup of database, html, and js events

--- quicklink.js
+++ quicklink.js
@@ -1,17 +1,56 @@
+Links =  new Mongo.Collection('links');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.links.helpers({
+    'linklist': function (){
+      return Links.find({}, {sort: {createdAt: -1}});
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.linkCounter.helpers({
+    'totalLinks': function(){
+      return Links.find().count();
+    }
+  });
+  Template.addLink.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var linkURL = $('[name="linkURL"]').val();
+      Links.insert({
+        linkurl: linkURL
+        createdAt: new Date();
+      });
+      $('[name="linkURL"]').val('');
+      console.log("New link '"+linkURL+"' added");
+    }
+  });
+  Template.linkItem.events({
+    'click .delete-link': function(event){
+      event.preventDefault();
+      var linkID = this._id;
+      var confirm = window.confirm("Delete this task?");
+      if(confirm){
+        Links.remove({ _id: linkID });
+      };
+    'keyup [name="linkItem"]': function(event){
+      if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+      }
+      else {
+        var linkID = this._id;
+        var newURL = $(event.target).val();
+        Links.update({
+          _id: linkID
+        },
+        {
+          $set: {
+            linkurl = newURL
+          }
+        });
+        console.log("Value of link "+linkID+" changed to '"+newURL+"'");
+      }
     }
   });
 }
